ba5c6a24032c5112f24085ecb4f34a642d637829
Marvin John <Marvinc.john.03@gmail.com>
2024-08-12 17:26:33 +0200

    Initial Commit

---
+++ RickNMorty/RickNMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickNMortyApp.swift
+//  RickNMorty
+//
+//  Created by Marvin John on 12.08.24.
+//
+
+import SwiftUI
+
+@main
+struct RickNMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
