6fc605920e4c4d1eedfae8a2e31b0030e7d316f1
Marvin John <Marvinc.john.03@gmail.com>
2024-08-13 18:23:28 +0200

    [Feature] Add Mock data

--- RickNMorty/Characters/Models/Character.swift
+++ RickNMorty/Characters/Models/Character.swift
@@ -18,4 +18,16 @@ struct Character: Codable{
     let status: String
     let species: String
     let gender: String
+    let image: String
+}
+
+struct MockData{
+    static let character = [
+        Character(id: 1,
+                  name: "Rick Sanchez",
+                  status: "Alive",
+                  species: "Human",
+                  gender: "Male",
+                  image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg")
+    ]
 }
